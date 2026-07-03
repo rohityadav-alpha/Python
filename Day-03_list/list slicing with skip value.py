51285361f9539dc108c581d12004b9dc38ad1554
rohityadav-next <yadavratan524@gmail.com>
2026-07-03 14:55:58 +0530

    List

--- Day-03_list/list slicing with skip value.py
+++ Day-03_list/list slicing with skip value.py
@@ -2,12 +2,13 @@
 list1=[1,7,3,"rohit yadav",9.2,True]
 
 print("list slicing with skip value")
-print(list1[::])
-print(list1[:5:2])
-print(list1[0::3])
-print(list1[0:5:])
-print(list1[0:5:4])
+print(list1[::]) #it will print all the values of list1
+print(list1[:5:2]) #it will print the values of list1 from index 0 to 4 with skip value 2
+print(list1[0::3]) #it will print the values of list1 from index 0 to last index with skip value 3
+print(list1[0:5:1]) #it will print the values of list1 from index 0 to 4 with skip value 1
+print(list1[0:5:4]) #it will print the values of list1 from index 0 to 4 with skip value 4
 
+#string slicing with skip value operation with list
 print("taken the name from list1")
 name = list1[3]
 print(name[::])
