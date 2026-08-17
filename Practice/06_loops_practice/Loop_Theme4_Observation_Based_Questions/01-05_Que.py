4f3f817339ac1adeb4831451dffeb71819b7a139
rohityadav-next <yadavratan524@gmail.com>
2026-08-17 20:35:30 +0530

    practice questions

--- Practice/06_loops_practice/Loop_Theme4_Observation_Based_Questions/01-05_Que.py
+++ Practice/06_loops_practice/Loop_Theme4_Observation_Based_Questions/01-05_Que.py
@@ -22,3 +22,12 @@ for i in range(1,6):
     if i==3:
         continue #befor this statement yhe loop is already sexecuted continue is a misleading statement it does not affect the while loop execution
 
+
+#3. What is the off-by-one error in this code meant to print the last 3 elements of a list, and how do you fix it?
+arr = [10, 20, 30, 40, 50]
+for i in range(len(arr) - 3, len(arr)): # this will be goes under negetive index if the length of the list is less then 3 
+    print(arr[i])
+# solution 
+arr = [10, 20]
+for i in range(max(0,len(arr) - 3),len(arr)): # to avoide the negetive indexing use max(0,len(arr)-3) it set max value upto zero it prevent the startig value becomes zero
+    print(arr[i])
