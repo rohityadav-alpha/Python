c484d3b85331e59510d5e7ff3abe0d8bea4abc4c
rohityadav-next <yadavratan524@gmail.com>
2026-08-17 21:30:38 +0530

    practice questions

--- Practice/01_string_operation_que/01_Easy/11_15_Que.py
+++ Practice/01_string_operation_que/01_Easy/11_15_Que.py
@@ -1,11 +1,6 @@
-# 11. Sort the Characters of a String Alphabetically
-import re
-
-from django.utils import text
-
-
 word=input("Enter the word: ")
 letter=input("Enter the leter: ")
+# 11. Sort the Characters of a String Alphabetically
 def sortString(w):
     nstr=""
     list1=list(w)
@@ -26,7 +21,7 @@ firstIndex(word,letter)
 
 # 13. Check Whether a String Contains Only Digits
 # solution 1 -- without .isdigit() method 
-char=input("Enter the char")
+char=input("Enter the character or digits")
 try :
     print(f" the string contain only digit{int(char)}")
 except ValueError:
@@ -46,18 +41,38 @@ def largest_sen(sentence):
     wd=""
     for word in sentence.split():
         current=0
-        for j in word:
+        for ch in word:
             current+=1
         if current>final:
             final=current
             wd=word
     return wd,final
 print(largest_sen(sen))
-print(sen.split())
 # solution 2
-words = text.split()
+words = sen.split()
 longest = ""
 for word in words:
     if len(word) > len(longest):
         longest = word
 print(longest) # programming
+
+
+# 15. Reverse the Order of Words in a Sentence
+# solution 1 -- without using [::-1] indexing method
+def rev_word(sentence):
+    final_list=[]
+    for word in sentence.split():
+        current_word=""
+        for ch in range(len(word)-1,-1,-1):
+            current_word+=word[ch]
+        final_list.append(current_word)
+    return (" ".join(final_list))
+print(rev_word(sen))
+# solution 2 with [::-1] indexing method
+def rev_words(sentence):
+    final_list=[]
+    for word in sentence.split():
+        final_list.append(word[::-1])
+    return (" ".join(final_list))
+print(rev_words(sen))
+
