dc7365c95ee76987418f17fae017a21ae3000992
rohityadav-next <yadavratan524@gmail.com>
2026-08-19 21:57:38 +0530

    practice questions

--- Practice/04_sets_operations_que.py/01_Easy/11-15_Que.py
+++ Practice/04_sets_operations_que.py/01_Easy/11-15_Que.py
@@ -16,3 +16,26 @@ def rem(s,e):
     s.discard(e)
     return s
 print(rem(s2,6))
+
+
+
+
+# 13. Count how many unique words are present in a sentence.
+sentence='''dear sir
+     my name is rohit yadav
+    i m writing this regard
+    my 5 days hollidays
+              thanks sirdear sir
+                   my name is rohit yadav
+                  i m writing this regard
+                  my 5 days hollidays
+                            thanks sirdear sir
+                                 my name is rohit yadav
+                                i m writing this regard
+                                my 5 days hollidays
+                                          thanks sir'''
+print(sentence.split())
+print(set(sentence.split()))
+def countunique(s):
+    return len(set(s.split()))
+print(countunique(sentence))
--- Practice/04_sets_operations_que.py/01_Easy/tempCodeRunnerFile.py
+++ Practice/04_sets_operations_que.py/01_Easy/tempCodeRunnerFile.py
@@ -1,12 +1,31 @@
-# 12. Remove an element from a set safely.
-s2={2,4,7,9,1,6,5,8,3}
-# solution 2 -- using discard() method
-def rem(s,e):
-    s.discard(e)
-    return s
-print(rem(s2,6))
-# solution 1 -- using remove() method
-def removeEl(s,e):
-    s.remove(e)
-    return s
-print(removeEl(s2,2))
+# 13. Count how many unique words are present in a sentence.
+sentence='''dear sir
+     my name is rohit yadav
+    i m writing this regard
+    my 5 days hollidays
+              thanks sirdear sir
+                   my name is rohit yadav
+                  i m writing this regard
+                  my 5 days hollidays
+                            thanks sirdear sir
+                                 my name is rohit yadav
+                                i m writing this regard
+                                my 5 days hollidays
+                                          thanks sir'''
+print(sentence.split())
+print(set(sentence.split()))
+def countunique(s):
+    return len(set(s.split()))
+print(countunique(sentence))
+
+def duplicate(s):
+    list1=s.split()
+    s1=set(list1)
+    nlist=[]
+    for el in list1:
+        if el not in s1:
+            pass
+        else:
+            nlist.append(el)
+    return set(nlist)
+print(duplicate(sentence))
