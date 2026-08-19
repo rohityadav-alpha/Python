c7e4d6be6aaba3485a291571653d083de66f7f45
rohityadav-next <yadavratan524@gmail.com>
2026-08-19 21:22:54 +0530

    practice questions

--- Practice/04_sets_operations_que.py/01_Easy/11-15_Que.py
+++ Practice/04_sets_operations_que.py/01_Easy/11-15_Que.py
@@ -4,3 +4,15 @@ def convert(s):
     return set(s)
 print(convert(s1))
 
+# 12. Remove an element from a set safely.
+s2={2,4,7,9,1,6,5,8,3}
+# solution 1 -- using remove() method
+def removeEl(s,e):
+    s.remove(e)
+    return s
+print(removeEl(s2,2))
+# solution 2 -- using discard() method
+def rem(s,e):
+    s.discard(e)
+    return s
+print(rem(s2,6))
