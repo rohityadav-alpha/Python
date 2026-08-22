b2eeb6c2cf4ea11e03107c63ee7381a27863ed55
rohityadav-next <yadavratan524@gmail.com>
2026-08-22 22:52:31 +0530

    practice questions

--- Practice/06_loops_practice/Loop_Theme2_Mathematics_Questions/01-05_Que.py
+++ Practice/06_loops_practice/Loop_Theme2_Mathematics_Questions/01-05_Que.py
@@ -28,3 +28,18 @@ def Count(n):
     return n1
 print(Count(num))
 
+
+# 3. [Easy] Reverse a number using a loop and check if it's a palindrome by comparing with original.
+num=int(input("enter thhe number :"))
+def palindrom(n):
+    temp=n
+    rev=0
+    while n>0:
+        digit=n%10
+        rev=rev*10+digit
+        n=n//10
+    if temp==rev:
+        return (f"the number is palindrom")
+    else:
+        return (f"the number is not palindrom")
+print(palindrom(num))
