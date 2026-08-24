dcc29efc0167ea00dd982f1b8fe1a36fab92cad9
rohityadav-next <yadavratan524@gmail.com>
2026-08-24 19:10:24 +0530

    practice questions

--- Practice/06_loops_practice/Loop_Theme2_Mathematics_Questions/01-05_Que.py
+++ Practice/06_loops_practice/Loop_Theme2_Mathematics_Questions/01-05_Que.py
@@ -55,3 +55,15 @@ def factorial(n):
     return fact
 print(factorial(5))
 
+
+# 5. [Easy] Check whether a number is prime using a loop.
+def prime(n):
+    if n>2:
+        for i in range(2,n):
+            if n%i==0:
+                return False
+    return True
+if prime(9):
+    print("the number is prime number")
+else:
+     print("the number is not prime number")
