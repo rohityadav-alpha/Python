0cd9a1099091772c82a1e36ec5f7bba03ea90069
rohityadav-next <yadavratan524@gmail.com>
2026-08-24 20:45:57 +0530

    practice questions

--- Practice/06_loops_practice/Loop_Theme2_Mathematics_Questions/06-10_Que.py
+++ Practice/06_loops_practice/Loop_Theme2_Mathematics_Questions/06-10_Que.py
@@ -17,5 +17,13 @@ print(gcd(15, 12)) # 6
 
 
 
-
+# 7. [Medium] Find the LCM of two numbers using a loop-based brute-force approach.
+# solution 1
+def LCM(a,b):
+    gcd=0
+    for i in range(1,min(a,b)+1):
+        if a%i==0 and b%i==0:
+            gcd=i
+    return (a*b)/gcd
+print(LCM(4,6))
 
