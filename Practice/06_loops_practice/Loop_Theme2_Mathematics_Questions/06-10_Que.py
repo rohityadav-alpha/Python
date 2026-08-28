456da374b84f48fe66c33d4da5d129f1fc2589f3
rohityadav-next <yadavratan524@gmail.com>
2026-08-28 21:09:47 +0530

    practice questions

--- Practice/06_loops_practice/Loop_Theme2_Mathematics_Questions/06-10_Que.py
+++ Practice/06_loops_practice/Loop_Theme2_Mathematics_Questions/06-10_Que.py
@@ -37,3 +37,20 @@ def lcm_bruteforce(a, b):
             return candidate
         candidate += larger
 print(lcm_bruteforce(4, 6)) # 12
+
+
+
+# 8. [Medium] Check whether a number is an Armstrong number (sum of cubes/nth powers of digits equals the number).
+def armstrong(n):
+    n1=str(n)
+    num=n
+    Sum=0
+    for i in n1:
+        digit=n%10
+        Sum+=digit**(len(n1))
+        n=n//10
+    if Sum==num:
+        return (f"the number is armstrong")
+    else:
+        return (f"the number is not armstrong")
+print(armstrong(9474))
--- Practice/06_loops_practice/Loop_Theme2_Mathematics_Questions/tempCodeRunnerFile.py
+++ Practice/06_loops_practice/Loop_Theme2_Mathematics_Questions/tempCodeRunnerFile.py
@@ -1,20 +1,14 @@
-# 7. [Medium] Find the LCM of two numbers using a loop-based brute-force approach.
-# solution 1
-def LCM(a,b):
-    gcd=0
-    for i in range(1,min(a,b)+1):
-        if a%i==0 and b%i==0:
-            gcd=i
-    return (a*b)/gcd
-print(LCM(4,6))
-# solution 2
-def lcm_bruteforce(a, b):
-    if a == 0 or b == 0:
-        return 0
-    larger = max(a, b)
-    candidate = larger
-    while True:
-        if candidate % a == 0 and candidate % b == 0:
-            return candidate
-        candidate += larger
-print(lcm_bruteforce(4, 6)) # 12
+# 8. [Medium] Check whether a number is an Armstrong number (sum of cubes/nth powers of digits equals the number).
+def armstrong(n):
+    n1=str(n)
+    num=n
+    Sum=0
+    for i in n1:
+        digit=n%10
+        Sum+=digit**(len(n1))
+        n=n//10
+    if Sum==num:
+        return (f"the number is armstrong")
+    else:
+        return (f"the number is not armstrong")
+print(armstrong(9474))
