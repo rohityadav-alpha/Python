e2113d7fc98e6ccf269f5e1494e921eb97122197
rohityadav-next <yadavratan524@gmail.com>
2026-09-06 14:16:39 +0530

    Practice Questions

--- Practice/tempCodeRunnerFile.py
+++ Practice/tempCodeRunnerFile.py
@@ -1,6 +1,17 @@
-n=10
-for i in range(n):
-    print("  "*(n-i),end="")
-    print("* "*(2*i-1),end="")
-    print(" "*(n-i))
-print()
+
+# FizzBuzz problem 
+# solution 1 with % operator
+def FizzBuss(num):
+    list3=[i*3 for i in range(num)]
+    list5=[i*3 for i in range(num)]
+    for i in range(1,num+1):
+        if i%3==0:
+            print("Fizz")
+        elif i%5==0:
+            print("Buzz")
+        elif i in list3 and i in list5:
+            print("FizzBuzz")
+        else:
+            print(i)
+    return list3,list5
+FizzBuss(15)
