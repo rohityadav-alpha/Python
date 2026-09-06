7443b45bf4a27d86c9e682651c6bf91ad5b745a1
rohityadav-next <yadavratan524@gmail.com>
2026-09-06 14:17:02 +0530

    Project Documentation

--- Practice/03_Que.py
+++ Practice/03_Que.py
@@ -26,21 +26,7 @@ print(GST.amountWithNoGst(amount,gst))
 
 
 # FizzBuzz problem 
-# solution 1 with % operator and list comprehension
-def FizzBuss(num):
-    list3=[i*3 for i in range(num)]
-    list5=[i*3 for i in range(num)]
-    for i in range(1,num+1):
-        if i%3==0:
-            print("Fizz")
-        elif i%5==0:
-            print("Buzz")
-        elif i in list3 and i in list5:
-            print("FizzBuzz")
-        else:
-            print(i)
-    return list3,list5
-FizzBuss(15)
+
 # solution 2 without %modulo operator
 def fizzBuzz(num):
     list3=[]
