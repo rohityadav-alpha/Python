461ac65afb948f9a5fcee52002e8486e511faf44
rohityadav-next <yadavratan524@gmail.com>
2026-09-13 13:53:39 +0530

    Practice Questions

--- Practice/01_string_operation_que/02_Medium/01-05_Que.py
+++ Practice/01_string_operation_que/02_Medium/01-05_Que.py
@@ -56,3 +56,22 @@ def removedup(s):
 print(removedup("roohitt"))
 
 
+
+# 5. Check Whether Two Strings Are Anagrams
+def anagrams(s1,s2):
+    def fre(s):
+        ndict={}
+        for ch1 in s:
+            count=0
+            for ch2 in s:
+                if ch1==ch2:
+                    count+=1
+            ndict[ch1]=count
+        return ndict
+    if len(s1)==len(s2) and fre(s1)==fre(s2) and set(s1)==set(s2):
+        return (f"the shtrings {s1} and {s2} are anagrams")
+    else:
+        return (f"the shtrings {s1} and {s2} are not anagrams")
+print(anagrams("listen","silent"))
+print(anagrams("hello","world"))
+print(anagrams("rohit","ohtri"))
--- Practice/01_string_operation_que/02_Medium/tempCodeRunnerFile.py
+++ Practice/01_string_operation_que/02_Medium/tempCodeRunnerFile.py
@@ -1,10 +1,18 @@
-# 4. Remove Duplicate Characters While Preserving Order
-def removedup(s):
-    word=""
-    for ch in s:
-        if ch not in word:
-            word+=ch
-        else:
-            continue
-    return word
-print(removedup("roohitt"))
+# 5. Check Whether Two Strings Are Anagrams
+def anagrams(s1,s2):
+    def fre(s):
+        ndict={}
+        for ch1 in s:
+            count=0
+            for ch2 in s:
+                if ch1==ch2:
+                    count+=1
+            ndict[ch1]=count
+        return ndict
+    if len(s1)==len(s2) and fre(s1)==fre(s2) and set(s1)==set(s2):
+        return (f"the shtrings {s1} and {s2} are anagrams")
+    else:
+        return (f"the shtrings {s1} and {s2} are not anagrams")
+print(anagrams("listen","silent"))
+print(anagrams("hello","world"))
+print(anagrams("rohit","ohtri"))
