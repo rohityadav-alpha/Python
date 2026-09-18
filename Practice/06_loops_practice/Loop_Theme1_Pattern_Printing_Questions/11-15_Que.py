bedb99751be5034fd0728f6f9efa556da3d53b48
rohityadav-next <yadavratan524@gmail.com>
2026-09-18 19:26:39 +0530

    Practice Questions

--- Practice/06_loops_practice/Loop_Theme1_Pattern_Printing_Questions/11-15_Que.py
+++ Practice/06_loops_practice/Loop_Theme1_Pattern_Printing_Questions/11-15_Que.py
@@ -15,7 +15,7 @@ def alt_pyramid(n):
       print("  "*(n-i),end="")
       bit=i%2
       for j in range(2*i-1):
-         print(bit ,end=" ")
+         print(bit,end=" ")
          bit=1-bit
       print()
 alt_pyramid(5)
@@ -108,3 +108,21 @@ parity_diamond(5)
 
 
 # 15. [Hard] Print a spiral-filled square matrix pattern with numbers 1 to n*n.
+# need to understand logic
+
+
+# 16. [Tricky] Print a butterfly pattern (two triangles joined at the base, mirrored horizontally).
+
+def butterfly(n):
+      def but(rng):
+         for i in rng:
+            print("* "*(i),end="")
+            for j in range(i+1,(n*2)):
+               if j==i or j==((n*2)-i) or j>=((n*2)-i):
+                  print("* ",end="")
+               else:
+                  print("  ",end="")
+            print()
+      but(range(1,n+1))
+      but(range(n,0,-1))
+butterfly(5)
--- Practice/06_loops_practice/Loop_Theme1_Pattern_Printing_Questions/tempCodeRunnerFile.py
+++ Practice/06_loops_practice/Loop_Theme1_Pattern_Printing_Questions/tempCodeRunnerFile.py
@@ -1,10 +1,15 @@
-# 9. [Medium] Print a floyd's triangle (continuously increasing numbers row-wise).
-def floyds_triangle(n):
-   num=1
-   for i in range(1,n+1):
-      row=[]
-      for j in range(i):
-         row.append(str(num))
-         num+=1
-      print("  ".join(row))
-floyds_triangle(5)
+# 16. [Tricky] Print a butterfly pattern (two triangles joined at the base, mirrored horizontally).
+
+def butterfly(n):
+      def but(rng):
+         for i in rng:
+            print("* "*(i),end="")
+            for j in range(i+1,(n*2)):
+               if j==i or j==((n*2)-i) or j>=((n*2)-i):
+                  print("* ",end="")
+               else:
+                  print("  ",end="")
+            print()
+      but(range(1,n+1))
+      but(range(n,0,-1))
+butterfly(5)
