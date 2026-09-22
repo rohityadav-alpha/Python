192d99bf8f3c39533072e6bf2c2cd42f847b9b16
rohityadav-next <yadavratan524@gmail.com>
2026-09-22 08:01:19 +0530

    Practice Questions

--- Practice/01_string_operation_que/02_Medium/06-10_Que.py
+++ Practice/01_string_operation_que/02_Medium/06-10_Que.py
@@ -11,4 +11,21 @@ def freqOfWord(sentences):
     return ndict
 print(freqOfWord("Count the Frequency of Each Word in Count the Frequency Word in Sentence"))
 
-
+# 7. Find the Most Frequent Character in a String
+def mostFreq(sentences):
+    ndict={}
+    sen=sentences.split()
+    for word in sen:
+        count=0
+        for word1 in sen:
+            if word==word1:
+                count+=1
+        ndict[word]=count
+    result={}
+    large=max(ndict.values())
+    for key,val in ndict.items():
+        if large==val:
+            result[key]=val
+    return result
+print(mostFreq("Count the Frequency of Each Word in Count the Frequency Word in Sentence Word"))
+    
